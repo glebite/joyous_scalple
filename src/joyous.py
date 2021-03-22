9d32cb22c6637ecfdb4764b625cb8a041e14dd10
glebite <glebite@gmail.com>
2021-03-22 19:24:43 -0400

    fleshing out base code and description of expected inputs and outputs

--- src/joyous.py
+++ src/joyous.py
@@ -1,9 +1,28 @@
+"""
+joyous.py - pcap to python bytes output
+
+python joyous.py -i input.pcap -o output.py
+
+Output should be something along the lines of:
+
+# IP src   192.168.1.5
+# IP sport 5000
+# IP dst   192.168.2.239
+# IP dport 6000
+data = bytes([0xa9, 0x03, 0x00 ...])
+"""
 import scapy
 import sys
 
 
+class Joyous(object):
+    def __init__(self):
+        pass
+
+
 def main(arguments):
     pass
+
 
 if __name__ == "__main__":
     main(sys.argv)
